ca46c787e53da743dddbc7080f99debea10164ca
Roman <marcina.roman@gmail.com>
2026-06-08 06:21:05 +0000

    update

--- git-push.sh
+++ git-push.sh
@@ -3,12 +3,12 @@ https://github.com/romeo0012/bpm-app.git
 echo "# bpm-app" >> README.md
 git init
 git add README.md
-git commit -m "first commit"
+git config --global user.name "Roman"
+git config --global user.email marcina.roman@gmail.com
+git add -A
+git commit -m "initial commit"
 git branch -M main
 git remote add origin https://github.com/romeo0012/bpm-app.git
-git push -u origin main
 
-git remote add origin https://github.com/romeo0012/bpm-app.git
-git branch -M main
 git push -u origin main
 
