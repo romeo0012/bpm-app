a78d5b21fd8821b8c440c123f5b20c055143c4c5
romeo0012 <58067066+romeo0012@users.noreply.github.com>
2026-08-24 08:02:41 +0200

    Update CamundaApplication.java

--- src/main/java/com/camunda/CamundaApplication.java
+++ src/main/java/com/camunda/CamundaApplication.java
@@ -1,13 +1,51 @@
 package com.camunda;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 //import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 //@EnableProcessApplication
 public class CamundaApplication {
 
+  @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void debugConfig() {
+        System.out.println("========== CONFIG DEBUG ==========");
+
+        System.out.println(
+            "camunda.bpm.database.schema-update = " +
+            env.getProperty("camunda.bpm.database.schema-update")
+        );
+
+        System.out.println(
+            "CAMUNDA_BPM_DATABASE_SCHEMA_UPDATE env = " +
+            System.getenv("CAMUNDA_BPM_DATABASE_SCHEMA_UPDATE")
+        );
+
+        System.out.println(
+            "SPRING_CONFIG_LOCATION env = " +
+            System.getenv("SPRING_CONFIG_LOCATION")
+        );
+
+        System.out.println(
+            "SPRING_CONFIG_ADDITIONAL_LOCATION env = " +
+            System.getenv("SPRING_CONFIG_ADDITIONAL_LOCATION")
+        );
+
+        System.out.println(
+            "DB_URL = " +
+            System.getenv("DB_URL")
+        );
+
+        System.out.println("==================================");
+    }
   public static void main(String... args) {
 
     String trustStore = System.getenv("JAVAX_NET_SSL_TRUSTSTORE");
